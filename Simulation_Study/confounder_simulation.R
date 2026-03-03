b0b6d3fcebee6d89c196cb93773ca62d851490e7
LLeimenstoll <161578774+LLeimenstoll@users.noreply.github.com>
2026-03-03 10:08:39 +0000

    Update confounder_simulation.R

--- Simulation_Study/confounder_simulation.R
+++ Simulation_Study/confounder_simulation.R
@@ -128,7 +128,7 @@ p1 <- ggplot(summary_df,
   theme(text = element_text(size = 40))
 p1
 
-ggsave("pics/confounder_test_sim_caseC.pdf", plot = p1, device = "pdf",
+ggsave("figures/confounder_test_sim_caseC.pdf", plot = p1, device = "pdf",
        width = 14, height = 8, path = simulation_path)
 
 # ---------------------------------------------------------------------------
@@ -163,5 +163,6 @@ p2 <- ggplot(summary_df,
   theme(text = element_text(size = 40))
 p2
 
-ggsave("pics/confounder_test_sim_caseD.pdf", plot = p2, device = "pdf",
+ggsave("figures/confounder_test_sim_caseD.pdf", plot = p2, device = "pdf",
+
        width = 14, height = 8, path = simulation_path)
